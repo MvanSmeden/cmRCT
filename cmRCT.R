bb29fb4d2efec46210da716e316465061ec2e028
MvanSmeden <36156780+MvanSmeden@users.noreply.github.com>
2018-04-10 14:27:41 +0200

    Add files via upload

--- cmRCT.R
+++ cmRCT.R
@@ -22,24 +22,36 @@ smrz <- function(x) c(mean(x),sd(x)/sqrt(length(x)))
 # ===================================
 # simulation function:
 
-sim.fun <- function(n.sim, n, R, delta.1, delta.2){
+sim.fun <- function(n.sim, n, block, R, delta.1, delta.2){
 	RESULTS.t <- array(NA, dim=c(n.sim,8,length(R)) )
 	RESULTS.m <- array(NA, dim=c(n.sim,8,length(R)) )
 
 	for (j in 1:length(R)){
 		for (i in 1:n.sim){
-		
+
+		if(block==TRUE){
+			n10 <- n
+			n11 <- n
+			n20 <- n
+			n21 <- n}
+		else{
+			n10 <- rbinom(1,2*n,.5)
+			n11 <- 2*n-n10
+			n20 <- rbinom(1,2*n,.5)
+			n21 <- 2*n-n20}
+
 		# option 1.	subjects can only participate in 1 trial
 			N <- round(4*n/R[j])	
 			ids <- 1:N 
 			dat <- rnorm(N)
-				sample.10 <- sample(ids,n)
+				sample.10 <- sample(ids,n10)
 				used.id <- sample.10
-				sample.11 <- sample(ids[-used.id],n)
+				sample.11 <- sample(ids[-used.id],n11)
 				used.id <- c(used.id,sample.11)
-				sample.20 <- sample(ids[-used.id],n)
+				sample.20 <- sample(ids[-used.id],n20)
 				used.id <- c(used.id,sample.20)
-				sample.21 <- sample(ids[-used.id],n)
+				sample.21 <- sample(ids[-used.id],n21)
+
 				used.id <- c(used.id,sample.21)
 				dat[sample.11] <- dat[sample.11] + delta.1
 				dat[sample.21] <- dat[sample.21] + delta.2
@@ -53,11 +65,11 @@ sim.fun <- function(n.sim, n, R, delta.1, delta.2){
 			N <- round(3*n/R[j])	
 			ids <- 1:N 
 			dat <- rnorm(N)
-				sample.11 	<- sample(ids,n)
-				sample.21 	<- sample(ids[-sample.11],n)
+				sample.11 	<- sample(ids,n11)
+				sample.21 	<- sample(ids[-sample.11],n21)
 				used.id 	<- c(sample.11,sample.21)
-				sample.10 	<- sample(ids[-used.id],n)
-				sample.20 	<- sample(ids[-used.id],n)
+				sample.10 	<- sample(ids[-used.id],n10)
+				sample.20 	<- sample(ids[-used.id],n20)
 				dat[sample.11] <- dat[sample.11] + delta.1
 				dat[sample.21] <- dat[sample.21] + delta.2
 				RESULTS.t[i,3,j]  <- t.test(dat[sample.11],dat[sample.10], var.equal=TRUE)$statistic
@@ -70,10 +82,10 @@ sim.fun <- function(n.sim, n, R, delta.1, delta.2){
 			N <- round(2*n*(1+sqrt(1-R[j]))/R[j])	
 			ids <- 1:N 
 			dat <- rnorm(N)
-				sample.11 	<- sample(ids,n)
-				sample.21 	<- sample(ids[-sample.11],n)
-				sample.10 	<- sample(ids[-sample.11],n)
-				sample.20 	<- sample(ids[-sample.21],n)
+				sample.11 	<- sample(ids,n11)
+				sample.21 	<- sample(ids[-sample.11],n21)
+				sample.10 	<- sample(ids[-sample.11],n10)
+				sample.20 	<- sample(ids[-sample.21],n20)
 				dat[sample.11] <- dat[sample.11] + delta.1
 				dat[sample.21] <- dat[sample.21] + delta.2
 				RESULTS.t[i,5,j]  <- t.test(dat[sample.11],dat[sample.10], var.equal=TRUE)$statistic
@@ -85,10 +97,10 @@ sim.fun <- function(n.sim, n, R, delta.1, delta.2){
 			N <- round(2*n/R[j])	
 			ids <- 1:N 
 			dat <- rnorm(N)
-				sample.10 <- sample(ids,n)
-				sample.11 <- sample(ids[-sample.10],n)
-				sample.20 <- sample(ids,n)
-				sample.21 <- sample(ids[-sample.20],n)
+				sample.10 <- sample(ids,n10)
+				sample.11 <- sample(ids[-sample.10],n11)
+				sample.20 <- sample(ids,n20)
+				sample.21 <- sample(ids[-sample.20],n21)
 				dat[sample.11] <- dat[sample.11] + delta.1
 				dat[sample.21] <- dat[sample.21] + delta.2
 				RESULTS.t[i,7,j]  <- t.test(dat[sample.11],dat[sample.10], var.equal=TRUE)$statistic
@@ -107,14 +119,15 @@ sim.fun <- function(n.sim, n, R, delta.1, delta.2){
 # no treatment effect in trial B (delta.2 = 0)
 
 set.seed(2505)
-n.sim 	<- 1e4
-n 		<- 50
-R		<- seq(.05,.95, by=0.05) 
-delta.1 	<- 0
-delta.2 	<- 0
+n.sim 	<- 1e5				# no. simulations
+n 		<- 50					# sample size per treatment arm
+R		<- seq(.05,.95, by=0.05) 	# proportion of cohort included in at least on of two trials
+delta.1 	<- 0					# treatment effect in trial 1
+delta.2 	<- 0					# treatment effect in trial 2
+block		<- TRUE				# block randomisation (TRUE/FALSE)
 
 # run simulation:
-sim.out <- sim.fun(n.sim, n, R, delta.1, delta.2)
+sim.out <- sim.fun(n.sim, n, block, R, delta.1, delta.2)
 
 # analyze results:
 CORS <- matrix(ncol=4,nrow=length(R))
@@ -126,15 +139,24 @@ for (j in 1:length(R)){
 	CORS[j,4] <- cor(M[,7], M[,8])
 	}
 
-df <- data.frame(option=rep(c("1","2","3","4"),each=19), R = rep(R,4), CORS = as.vector(CORS))
+cor.lower <- CORS - qnorm(0.975)*sqrt((1-CORS^2) / (n.sim-2)) 
+cor.upper <- CORS + qnorm(0.975)*sqrt((1-CORS^2) / (n.sim-2)) 
+
+df <- data.frame(option=rep(c("1","2","3","4"),each=19), R = rep(R,4), CORS = as.vector(CORS), 
+		cor.lower = as.vector(cor.lower), cor.upper = as.vector(cor.upper))
+
 ggplot(data=df, aes(x=R,y=CORS, group=option)) +
 	geom_line(aes(linetype=option)) +
+	geom_ribbon(aes(ymin=cor.lower, ymax=cor.upper), alpha=0.15) + 
 	scale_linetype_manual(values=c("solid","longdash","twodash", "dotted")) +
-	labs(x='proportion of cohort included in trials',y='correlation between trial results') +
-	scale_y_continuous(limits=c(-.55, .55))
+	labs(x='proportion of cohort included in at least one of two trials',y='correlation between trial results') +
+	scale_y_continuous(limits=c(-.6, .6)) +
+	annotate("text", x = rep(0.9,4), y = c(.05,.3,-.3,-.05), label = c("1","2","3","4")) +
+	theme_bw() 
 
 # ggsave("Figure1.tiff", device="tiff",dpi=200)
 
+# ==============================================================
 # ==============================================================
 # rerun n.sim = 1e6
 # for R = 0.5 
@@ -142,15 +164,23 @@ ggplot(data=df, aes(x=R,y=CORS, group=option)) +
 # no treatment effect in trial B (delta.2 = 0)
 # to assess conditional type I error:
 
+TABLE.2 		<- matrix(ncol=5,nrow=12)
+rownames(TABLE.2) <- c("option 1. unconditional", "p<0.05", "p>=0.05",
+				"option 2. unconditional", "p<0.05", "p>=0.05",
+				"option 3. unconditional", "p<0.05", "p>=0.05",
+				"option 4. unconditional", "p<0.05", "p>=0.05")
+colnames(TABLE.2) <- c("type I error","bias","ESE","bias/ESE","type II error")
+
 set.seed(2505)
 n.sim 	<- 1e6
 n 		<- 50
 R		<- 0.5
 delta.1 	<- 0
 delta.2 	<- 0
+block		<- TRUE
 
 # run simulation:
-sim.out <- sim.fun(n.sim, n, R, delta.1, delta.2)
+sim.out <- sim.fun(n.sim, n, block, R, delta.1, delta.2)
 
 # analyze results:
 t.values 	<- sim.out$t[,,1]
@@ -159,30 +189,40 @@ p.values 	<- 1-pt(t.values,df)
 sign.p 	<- 1*(p.values < 0.05)
 m.values	<- sim.out$m[,,1]
 
-TABLE.2 	<- matrix(ncol=4,nrow=8)
-TABLE.2[1,1] <- mean( sign.p[,2][sign.p[,1]==1])
-TABLE.2[2,1] <- mean( sign.p[,2][sign.p[,1]==0])
-TABLE.2[3,1] <- mean( sign.p[,4][sign.p[,3]==1])
-TABLE.2[4,1] <- mean( sign.p[,4][sign.p[,3]==0])
-TABLE.2[5,1] <- mean( sign.p[,6][sign.p[,5]==1])
-TABLE.2[6,1] <- mean( sign.p[,6][sign.p[,5]==0])
-TABLE.2[7,1] <- mean( sign.p[,8][sign.p[,7]==1])
-TABLE.2[8,1] <- mean( sign.p[,8][sign.p[,7]==0])
+TABLE.2[1,1] <- mean( sign.p[,2])
+TABLE.2[2,1] <- mean( sign.p[,2][sign.p[,1]==1])
+TABLE.2[3,1] <- mean( sign.p[,2][sign.p[,1]==0])
+TABLE.2[4,1] <- mean( sign.p[,4])
+TABLE.2[5,1] <- mean( sign.p[,4][sign.p[,3]==1])
+TABLE.2[6,1] <- mean( sign.p[,4][sign.p[,3]==0])
+TABLE.2[7,1] <- mean( sign.p[,6])
+TABLE.2[8,1] <- mean( sign.p[,6][sign.p[,5]==1])
+TABLE.2[9,1] <- mean( sign.p[,6][sign.p[,5]==0])
+TABLE.2[10,1] <- mean( sign.p[,8])
+TABLE.2[11,1] <- mean( sign.p[,8][sign.p[,7]==1])
+TABLE.2[12,1] <- mean( sign.p[,8][sign.p[,7]==0])
 
-TABLE.2[1,2:3] <- smrz(m.values[,2][sign.p[,1]==1])
-TABLE.2[2,2:3] <- smrz(m.values[,2][sign.p[,1]==0])
-TABLE.2[3,2:3] <- smrz(m.values[,4][sign.p[,3]==1])
-TABLE.2[4,2:3] <- smrz(m.values[,4][sign.p[,3]==0])
-TABLE.2[5,2:3] <- smrz(m.values[,6][sign.p[,5]==1])
-TABLE.2[6,2:3] <- smrz(m.values[,6][sign.p[,5]==0])
-TABLE.2[7,2:3] <- smrz(m.values[,8][sign.p[,7]==1])
-TABLE.2[8,2:3] <- smrz(m.values[,8][sign.p[,7]==0])
+TABLE.2[1,2:3] <- smrz(m.values[,2])
+TABLE.2[2,2:3] <- smrz(m.values[,2][sign.p[,1]==1])
+TABLE.2[3,2:3] <- smrz(m.values[,2][sign.p[,1]==0])
+TABLE.2[4,2:3] <- smrz(m.values[,4])
+TABLE.2[5,2:3] <- smrz(m.values[,4][sign.p[,3]==1])
+TABLE.2[6,2:3] <- smrz(m.values[,4][sign.p[,3]==0])
+TABLE.2[7,2:3] <- smrz(m.values[,6])
+TABLE.2[8,2:3] <- smrz(m.values[,6][sign.p[,5]==1])
+TABLE.2[9,2:3] <- smrz(m.values[,6][sign.p[,5]==0])
+TABLE.2[10,2:3] <- smrz(m.values[,8])
+TABLE.2[11,2:3] <- smrz(m.values[,8][sign.p[,7]==1])
+TABLE.2[12,2:3] <- smrz(m.values[,8][sign.p[,7]==0])
+
+TABLE.2[,4] <- TABLE.2[,2]/TABLE.2[,3]
 
 # ====================================================================
 # === SCENARIO 2 ===
+# rerun n.sim = 1e6
+# for R = 0.5 
 # no treatment effect in trial A (delta.1 = 0)
 # treatment effect in trial B (delta.2 = 0.56)
-# for R = 0.5: 
 # to assess conditional type II error:
 
 set.seed(2505)
@@ -191,9 +231,10 @@ n 		<- 50
 R		<- 0.5
 delta.1 	<- 0
 delta.2 	<- sqrt(2*(qnorm(0.95)+qnorm(.8))^2/n)
+block		<- TRUE
 
 # run simulation:
-sim.out <- sim.fun(n.sim, n, R, delta.1, delta.2)
+sim.out <- sim.fun(n.sim, n, block, R, delta.1, delta.2)
 
 # analyze results:
 t.values 	<- sim.out$t[,,1]
@@ -201,23 +242,22 @@ df 		<- 2*n - 2
 p.values 	<- 1-pt(t.values,df)
 sign.p 	<- 1*(p.values < 0.05)
 
-TABLE.2[1,4] <- mean(1- sign.p[,2][sign.p[,1]==1])
-TABLE.2[2,4] <- mean(1- sign.p[,2][sign.p[,1]==0])
-TABLE.2[3,4] <- mean(1- sign.p[,4][sign.p[,3]==1])
-TABLE.2[4,4] <- mean(1- sign.p[,4][sign.p[,3]==0])
-TABLE.2[5,4] <- mean(1- sign.p[,6][sign.p[,5]==1])
-TABLE.2[6,4] <- mean(1- sign.p[,6][sign.p[,5]==0])
-TABLE.2[7,4] <- mean(1- sign.p[,8][sign.p[,7]==1])
-TABLE.2[8,4] <- mean(1- sign.p[,8][sign.p[,7]==0])
+TABLE.2[1,5] <- mean(1- sign.p[,2])
+TABLE.2[2,5] <- mean(1- sign.p[,2][sign.p[,1]==1])
+TABLE.2[3,5] <- mean(1- sign.p[,2][sign.p[,1]==0])
+TABLE.2[4,5] <- mean(1- sign.p[,4])
+TABLE.2[5,5] <- mean(1- sign.p[,4][sign.p[,3]==1])
+TABLE.2[6,5] <- mean(1- sign.p[,4][sign.p[,3]==0])
+TABLE.2[7,5] <- mean(1- sign.p[,6])
+TABLE.2[8,5] <- mean(1- sign.p[,6][sign.p[,5]==1])
+TABLE.2[9,5] <- mean(1- sign.p[,6][sign.p[,5]==0])
+TABLE.2[10,5] <- mean(1- sign.p[,8])
+TABLE.2[11,5] <- mean(1- sign.p[,8][sign.p[,7]==1])
+TABLE.2[12,5] <- mean(1- sign.p[,8][sign.p[,7]==0])
 
-# =========================================================================
-rownames(TABLE.2) <- c("option 1. p<0.05", "p>=0.05",
-				"option 2. p<0.05", "p>=0.05",
-				"option 3. p<0.05", "p>=0.05",
-				"option 4. p<0.05", "p>=0.05")
-colnames(TABLE.2) <- c("type I error","bias","ESE","type II error")
+TABLE.2
 
-rnd(TABLE.2,c(3,4,4,3))
+rnd(TABLE.2,c(3,4,4,2,3))
 
 # ========================================================================
 # END
